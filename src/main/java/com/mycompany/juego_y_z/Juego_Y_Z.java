ee8be939df0b809639a02d3eeb17c8b1c735957e
isoto <samymaty1420@gmail.com>
2023-05-18 22:24:03 -0500

    tercer repositorio

---
+++ src/main/java/com/mycompany/juego_y_z/Juego_Y_Z.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.juego_y_z;
+
+/**
+ *
+ * @author Isoto
+ */
+public class Juego_Y_Z {
+
+    public static void main(String[] args) {
+        
+    }
+}
