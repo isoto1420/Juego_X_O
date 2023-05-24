a2fc500fd08bd4d052dbb84daead17afce2b95eb
isoto1420 <118865588+isoto1420@users.noreply.github.com>
2023-05-24 18:43:20 -0500

    Update Y_Z.java

--- src/main/java/Y_Z.java
+++ src/main/java/Y_Z.java
@@ -2,7 +2,7 @@
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
+//comentario de branch
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
